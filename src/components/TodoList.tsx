22bc364eee44a0b44d69ba7f8a904aeb11916656
samTang1a <samtang@samdeimac.home>
2023-08-30 00:03:12 -0400

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class ToDoList extends Component {
+
+  todos: string[] = ["Buy groceries", "Clean the house", "Walk the dog"]
+
+  render() {
+    return <h1>Hello, !</h1>;
+    
+  }
+}
+
