bf18df2710b273bdc8e6c69c93d775290f69e8ed
samTang1a <samtang2@lt06.local>
2023-08-30 15:11:17 -0400

    dev commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,8 +5,14 @@ export default class ToDoList extends Component {
   todos: string[] = ["Buy groceries", "Clean the house", "Walk the dog"]
 
   render() {
-    return <h1>Hello, !</h1>;
-    
+    return (
+      <>
+        <ul className="list-group">
+          {this.todos.map(todo =>
+            (<li className="list-group-item">{todo}</li>)
+          )}
+        </ul>
+      </>
+    )
   }
 }
-
